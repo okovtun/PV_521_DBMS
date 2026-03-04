4dd18c3ddf0d6adbc1e2b2cd64431bdf6bf0fcee
User <User@DESKTOP-QHG18FL>
2026-03-04 20:31:28 +0300

    GetNextLearningDate DONE

--- SQL/Functions/SQLQuery0-Check.sql
+++ SQL/Functions/SQLQuery0-Check.sql
@@ -2,4 +2,7 @@
 USE PV_521_Import;
 SET DATEFIRST 1;
 
-PRINT dbo.GetNextLearningDay(N'PV_521', N'2026-03-02')
+EXEC sp_SelectScheduleFor N'PV_521'
+--PRINT dbo.GetNextLearningDay(N'PV_521', DEFAULT);
+PRINT dbo.GetNextLearningDay(N'PV_521', N'2026-03-05');
+PRINT dbo.GetNextLearningDate(N'SPU_411', N'2026-03-07');
